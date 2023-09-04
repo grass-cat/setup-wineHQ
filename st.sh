d096af56aa19b396ad514a860bb72d95fb6bdf0b
grass-cat <130976659+grass-cat@users.noreply.github.com>
2023-09-04 17:24:25 +0700

    Update st.sh

--- st.sh
+++ st.sh
@@ -14,6 +14,13 @@ curl -L https://raw.githubusercontent.com/grass-cat/setup/main/00.sh | bash
 
 curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/winestp.sh | bash
 
+xfconf-query -c xfce4-panel -p /panels/panel-1/size -t int -s 28
+
+ls /usr/share/themes/
+xfconf-query -c xfwm4 -p /general/theme -s "Default-xhdpi"
+
+xfconf-query -c xsettings -p /Gtk/FontConfig/TitleFontSize -s "12"
+
 #PEAZIP INSTALL
 wget https://github.com/peazip/PeaZip/releases/download/9.1.0/peazip_9.1.0.LINUX.GTK2-1_amd64.deb
 

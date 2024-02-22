49ed95120869e4506bbf7acb9d813ae09a75bee1
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 09:29:54 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -10,17 +10,18 @@ read -p "CHOICE NUMBER (1 - 3): " option
 case $option in
     1)
         echo "Running Option 1..."
-        # curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/ub.sh | bash
+        curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/ub.sh | bash
         ;;
     2)
         echo "Running Option 2..."
-        # curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wi.sh | bash
+        curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wi.sh | bash
         ;;
     3)
         echo "Running Option 3..."
-        # curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wu.sh | bash
+        curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wu.sh | bash
         ;;
     *)
         echo "INVALID CHOICE."
+        exit 1
         ;;
 esac

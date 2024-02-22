b79e68221fa0e6c0617dd95dc71f9b6d4d3bad22
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 09:29:31 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Download
-wget https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/mn.sh -O temp_script.sh
+wget https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/mn.sh -O mn
 
 # PERMISSION
-chmod +x temp_script.sh
+chmod +x mn
 
 # Execute
-./temp_script.sh
-
-# Clean
-rm temp_script.sh
+./mn

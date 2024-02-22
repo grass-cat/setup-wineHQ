2b99f0cfcc4aa4d503df64db03aee3e79243f1c7
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 09:51:59 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -6,5 +6,5 @@ wget https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/mn.sh -O mn
 # PERMISSION
 chmod +x mn
 
-gnome-terminal -- bash -c "bash ./mn; exec bash"
+x-terminal-emulator -e "bash ./mn"
 exit

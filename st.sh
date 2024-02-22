0efa28b183f82c9416b1d374ff47c2c85d5b5530
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 08:58:13 +0700

    Update st.sh

--- st.sh
+++ st.sh
@@ -9,14 +9,6 @@ echo "user ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR="tee -a" visudo
 # LIST
 wget -O mn.sh https://github.com/grass-cat/setup-wineHQ/raw/main/mn.sh > /dev/null 2>&1
 chmod +x mn.sh
+#
+clear
 ./mn.sh
-
-function goto
-{
-    label=$1
-    cd 
-    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
-          grep -v ':$')
-    eval "$cmd"
-    exit
-}

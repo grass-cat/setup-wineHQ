4193c487bf903598ff3d59f962135d54763f87a6
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 08:42:20 +0700

    Update st.sh

--- st.sh
+++ st.sh
@@ -5,9 +5,11 @@ echo '123456' | sudo -S yourCommand
 # NO PASSWORD
 sudo su
 echo "user ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
-##
+
+# LIST
 wget https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/mn.sh
-sudo chmod +x mn.sh
-##
 cd
+sudo chmod +x mn.sh
+
+#
 sudo bash ./mn.sh

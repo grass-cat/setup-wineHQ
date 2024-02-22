a22eff2101c1d798a298910beed16b5e4131e428
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-22 09:04:29 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -7,18 +7,15 @@ echo "3. SETUP UBUNTU & UPDATE WINE"
 
 read -p "CHOICE NUMBER (1 - 3): " option
 
-if ! [[ $option =~ ^[1-3]$ ]]; then
-    echo "INVALID INPUT. Please enter a number between 1 and 3."
-    exit 1
-fi
-
-if [ "$option" -eq 1 ]; then
-    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/ub.sh | bash
+if [ $option -eq 1 ]; then
+    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/ub.sh | source /dev/stdin
     echo "SETUP UBUNTU"
-elif [ "$option" -eq 2 ]; then
-    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wi.sh | bash
+elif [ $option -eq 2 ]; then
+    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wi.sh | source /dev/stdin
     echo "SETUP UBUNTU & WINE"
-elif [ "$option" -eq 3 ]; then
-    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wu.sh | bash
+elif [ $option -eq 3 ]; then
+    curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wu.sh | source /dev/stdin
     echo "SETUP UBUNTU & UPDATE WINE"
+else
+    echo "INVALID."
 fi

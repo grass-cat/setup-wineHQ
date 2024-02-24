359b9e5ef90a484c93a8be1d569087589ce0baca
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-24 23:24:38 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -23,6 +23,6 @@ exec fish
 
 clear
 ######
-echo -e "\e[33mTYPE \"./mn\" TO RUN SCRIPT AGAIN.\e[0m"
+echo -e "\033[1;32mTYPE \033[0m\033[1;33m./mn \033[0m\033[1;32mTO RUN SCRIPT AGAIN\033[0m"
 ######
 exit

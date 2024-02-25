9838f83ec2de2c2b5d4609a52aa6d46ce0f4570d
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-25 08:16:05 +0700

    Update st.sh

--- st.sh
+++ st.sh
@@ -9,9 +9,10 @@ echo "user ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR="tee -a" visudo
 sudo apt update
 sudo apt-get install -y fish
 echo "exec fish" >> ~/.bashrc
+
 source ~/.bashrc
-clear
-exec fish
 
 # SCRIPT
 curl -L https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/dl.sh | bash
+
+exec fish

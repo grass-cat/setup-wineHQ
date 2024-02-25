4ad6afef1347ffd19272287d772dd5332ec0ee0a
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-25 08:08:53 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -8,21 +8,8 @@ chmod +x mn
 
 # 
 x-terminal-emulator -e "bash ./mn"
-
-# FISH
-sudo apt update
-sudo apt-get install -y fish
-
-#
-echo "exec fish" >> ~/.bashrc
-source ~/.bashrc
-
-#
-exec fish
-
-#
-echo -e "\e[31mDone\e[0m"
 clear
+echo -e "\e[31mDone\e[0m"
 ######
 echo -e "\033[1;32mTYPE \033[0m\033[1;33m./mn \033[0m\033[1;32mTO RUN SCRIPT AGAIN\033[0m"
 ######

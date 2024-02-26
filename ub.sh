8dbb877f595dcd4c6ec8e2cc5fe8a31840cc99e2
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-26 14:36:23 +0700

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -58,8 +58,6 @@ sudo apt-get autoclean
 sudo apt-get autoremove
 sudo apt update
 
-# FISH
-sudo apt-get install -y fish
-echo "exec fish" >> ~/.bashrc
-source ~/.bashrc
-exec fish
+# Done
+clear
+echo -e "\e[31mDone\e[0m"

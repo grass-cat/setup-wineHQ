458b4e258a8b6e746b5901568ec8a23ff18ddd22
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-02-27 11:18:11 +0700

    Update wi.sh

--- wi.sh
+++ wi.sh
@@ -6,9 +6,8 @@ sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-
 sudo apt-get update
-sudo apt update && sudo apt install --install-recommends winehq-stable -y
+sudo apt-get install --install-recommends winehq-stable -y
 
 # WINE UPDATE
 curl -sSL https://raw.githubusercontent.com/grass-cat/setup-wineHQ/main/wu.sh | bash

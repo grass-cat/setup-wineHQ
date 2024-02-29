aaeb83d809df18f5fa4bd59424a3740c552f503d
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-03-01 00:20:48 +0700

    Update wi.sh

--- wi.sh
+++ wi.sh
@@ -5,6 +5,7 @@
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 
 sudo apt update
 sudo apt-get install --install-recommends winehq-stable -y

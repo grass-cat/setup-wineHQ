f99984eaf5f23b937edf25780701eaa1436106b2
grass-cat <130976659+grass-cat@users.noreply.github.com>
2024-03-05 09:34:31 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -9,8 +9,7 @@ chmod +x mn
 # 
 x-terminal-emulator -e "bash ./mn"
 clear
-echo -e "\e[31mDone\e[0m"
-######
+
+#
 echo -e "\033[1;32mTYPE \033[0m\033[1;33m./mn \033[0m\033[1;32mTO RUN SCRIPT AGAIN\033[0m"
-######
 exit
